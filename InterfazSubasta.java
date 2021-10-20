a13754b7ddfe1273baa3f88f97880a680e42bb56
V. A. Canales L <victorcanlima@gmail.com>
2021-10-20 16:20:22 -0500

    Implemented server side remote behaviour

---
+++ InterfazSubasta.java
@@ -0,0 +1,23 @@
+
+import java.rmi.*;
+import java.util.Vector;
+
+public interface InterfazSubasta extends Remote {
+
+    public boolean registraUsuario( String nombre ) throws RemoteException;
+
+    public boolean agregaProductoALaVenta(
+        String vendedor, 
+        String producto,
+        float precioInicial 
+    ) throws RemoteException;
+    
+    public boolean agregaOferta(
+        String comprador, 
+        String producto, 
+        float monto 
+    ) throws RemoteException;
+    
+    public Vector<InformacionProducto> obtieneCatalogo() throws RemoteException;
+
+}
--- SubastaModelo.java
+++ SubastaModelo.java
@@ -1,21 +1,26 @@
-
+import java.rmi.*;
+import java.rmi.server.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.Hashtable;
 import java.util.Vector;
 
-public class SubastaModelo {
+
+public class SubastaModelo extends UnicastRemoteObject implements InterfazSubasta{
 
     Hashtable<String,String> usuarios;
     Hashtable<String,InformacionProducto> productos;
     Hashtable<String,InformacionOferta> ofertas;
 
-    public SubastaModelo() {
+    public SubastaModelo() throws RemoteException {
 
+        super();
         usuarios = new Hashtable<String,String>();
         productos = new Hashtable<String,InformacionProducto>();
         ofertas = new Hashtable<String,InformacionOferta>();
     }
 
-    public boolean registraUsuario( String nombre ) {
+    public boolean registraUsuario( String nombre ) throws RemoteException {
 
         if( !usuarios.containsKey(nombre) ) {
 
@@ -32,7 +37,7 @@ public class SubastaModelo {
         String vendedor, 
         String producto,
         float precioInicial 
-    ) {
+    ) throws RemoteException {
         if( !productos.containsKey(producto) ) {
 
             System.out.println( "Agregando un nuevo producto: " + producto );
@@ -51,7 +56,7 @@ public class SubastaModelo {
         String comprador, 
         String producto, 
         float monto 
-    ) {
+    ) throws RemoteException {
 
         if( productos.containsKey(producto) ) {
 
@@ -75,9 +80,30 @@ public class SubastaModelo {
             return false;
     }
 
-    public Vector<InformacionProducto> obtieneCatalogo() {
+    public Vector<InformacionProducto> obtieneCatalogo() throws RemoteException {
         Vector<InformacionProducto> resultado;
         resultado = new Vector<InformacionProducto>( productos.values() );
         return resultado;
+    }
+
+    public static void main(String args[]) {
+	
+        try {
+
+            SubastaModelo obj = new SubastaModelo();
+            InterfazSubasta stub = (InterfazSubasta) UnicastRemoteObject.exportObject(obj, 0);
+    
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("SubastaModelo", stub);
+    
+            System.err.println("Server ready");
+
+        } catch (Exception e) {
+
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+
+        }
     }
 }
