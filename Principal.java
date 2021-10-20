b16f47c222108fe774f80b16e1ffb9dc14164aca
V. A. Canales L <victorcanlima@gmail.com>
2021-10-20 17:10:30 -0500

    Implemented client side remote behaviour

--- Principal.java
+++ Principal.java
@@ -1,16 +1,31 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Principal {
 
     public static void main( String args[] ) {
 
         SubastaVista vista;
         SubastaControlador controlador;
-        SubastaModelo modelo;
+        InterfazSubasta modelo;
 
-        vista = new SubastaVista();
-        modelo = new SubastaModelo();
-        controlador = new SubastaControlador( vista, modelo );
+        String host = (args.length < 1) ? null : args[0];
 
-        vista.asignarActionListener( controlador );
-        vista.asignarListSelectionListener( controlador );
+        try {
+            
+            Registry registry = LocateRegistry.getRegistry(host);
+            InterfazSubasta stub = (InterfazSubasta) registry.lookup("Hello");
+
+            vista = new SubastaVista();
+            modelo = stub;
+            controlador = new SubastaControlador( vista, modelo );
+    
+            vista.asignarActionListener( controlador );
+            vista.asignarListSelectionListener( controlador );
+
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+        }
     }
 }
